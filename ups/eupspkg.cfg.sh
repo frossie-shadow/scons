7d465135ccd95552bf85f1e33574a439b0a8b0a8
Tim Jenness <tjenness@lsst.org>
2016-07-22 14:54:16 -0700

    Install the ups directory

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -9,4 +9,10 @@ install()
 {
     clean_old_install
     python2.7 setup.py install --prefix "$PREFIX" --standalone-lib
+
+    # Install ups if the native build system hasn't done it already.
+    # These lines come from the default install function in eupspkg
+    if [[ -d "ups" && ! -d "$PREFIX/ups" ]]; then
+        install_ups
+    fi
 }
